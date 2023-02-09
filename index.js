664fd0c8c54f67bd841c37dd66dc50243c5c4b83
dvidrine <Dustin.vidrine@cgifederal.com>
2023-02-09 12:40:08 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const combined = [];
+let sortedNames = [];
+let sortedIds = [];
+let urlArray = [];
+const tableBody = document.querySelector('#table-body');
+const tableHeadings = document.querySelectorAll('.table-heading');
+const blurOverlay = document.querySelector("#overlay");
+const loader = document.querySelector('#loader');
+
+
+
+tableHeadings.forEach(heading => {
+    heading.addEventListener('click', (e) => {    
+    if(e.currentTarget.textContent === 'Name') {
+        sortTable('name');       
+    }else if (e.currentTarget.textContent === 'ID') {
+        sortTable('id')
+    } 
+})
+})
+
+function getRepos() {
+    const user = prompt("Enter Github username: ");
+    // const user = 'kruzik762'
+    fetch(`https://api.github.com/users/${user}/repos`)
+        .then(response => {
+            blurOverlay.classList.add('overlay');
+            loader.classList.add('loader');
+            if(!response.ok){
+                throw new Error("User Profile not found")    
+            }
+            return response.json()
+
+        })
+        .then(data => {
+            if(!data){
+                throw new Error("User Profile not found")    
+            }
+            
+            data.forEach((repo,index) => {
+            buildTable(repo.name, repo.id, repo.html_url);
+            populateCombinedArray(repo.name,repo.id, repo.html_url,index);
+           });
+        }).catch(error => {
+
+            console.error(error);
+        }).finally( () => {
+            setTimeout( () => {
+                loader.classList.remove('loader');
+                blurOverlay.classList.remove('overlay');
+
+            },1200)
+        })
+}
+
+function buildTable(name, id, link) {   
+    tableBody.insertAdjacentHTML('beforeend',`
+    <tr>
+    <td>${name}</td>
+    <td>${id}</td>
+    <td><a href="${link}">View</a></td>
+    </tr>`
+    );
+}
+let idsAscending = true;
+let namesAscending = true;
+
+
+function sortTable(sortBy='name') {
+  
+    if (sortBy === 'name') {
+        
+        tableBody.innerHTML = '';              
+        if(namesAscending) {
+            combined.sort((a,b) => a[0].toLowerCase() === b[0].toLowerCase() ? 0 : a[0].toLowerCase() > b[0].toLowerCase() ? -1 : 1);
+            namesAscending = !namesAscending;
+            sortCombined();
+            console.log(combined)
+        } else {
+            combined.sort((a,b) => a[0].toLowerCase() === b[0].toLowerCase() ? 0 : a[0].toLowerCase() < b[0].toLowerCase() ? -1 : 1);
+            namesAscending = !namesAscending;
+            sortCombined();
+            console.log(combined)
+        }      
+
+    } else if (sortBy === 'id') {
+        tableBody.innerHTML = '';
+        // populateCombinedArray();
+        if(idsAscending) {
+            combined.sort((a,b) => a[1] - b[1]);
+            idsAscending = !idsAscending;
+        } else {
+            combined.sort((a,b) => b[1] - a[1]);
+            idsAscending = !idsAscending;            
+        }        
+        sortCombined(combined);
+    }   
+    
+    function sortCombined(){
+        sortedIds = [];
+        sortedNames = [];
+        urlArray = []
+        combined.forEach((item,index) => {
+            sortedNames.push(item[0]);
+            sortedIds.push(item[1]);
+            urlArray.push(item[2])
+            buildTable(sortedNames[index], sortedIds[index], urlArray[index])
+        })        
+    }    
+}
+function populateCombinedArray(name, id, url, index) {    
+    combined.push([name,Number(id), url]);
+}
+getRepos();
+ 
